b433f269eea55a8ce6f18ae60c5c2dfb8bec9f34
harshvardhansingh1129-rgb <harshvardhansingh1129@gmail.com>
2026-08-10 21:42:16 +0530

    Day 1: solve sum of two numbers

---
+++ sum.c
@@ -0,0 +1,12 @@
+ //write a program to input to numbers and display their sum.
+ #include<stdio.h>
+ int main (){
+    int a,b,sum;
+     printf("enter the first number:");
+     scanf("%d",&a);
+     printf("enter the second number:");
+     scanf("%d",&b);
+     sum=a+b;
+     printf("sum= %d",sum);
+     return 0;
+}
