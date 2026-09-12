673094c919c5997908be701478204f13bb14005d
harshvardhansingh1129-rgb <harshvardhansingh1129@gmail.com>
2026-09-12 17:59:11 +0530

    Add files via upload

---
+++ m.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, pos;
+
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter position to delete: ");
+    scanf("%d", &pos);
+
+    // Shift elements to the left
+    for (i = pos - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    n--;
+
+    printf("Array after deletion: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
