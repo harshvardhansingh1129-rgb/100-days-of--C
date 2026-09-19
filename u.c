7e8ede0fed59876c8ae7ecc10498d820bd2c8945
harshvardhansingh1129-rgb <harshvardhansingh1129@gmail.com>
2026-09-19 17:29:57 +0530

    Add files via upload

--- u.c
+++ u.c
@@ -3,16 +3,17 @@
 int main()
 {
     char str[100];
+    int count = 0;
 
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
 
-    printf("Characters are:\n");
-
-    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++)
+    while (str[count] != '\0' && str[count] != '\n')
     {
-        printf("%c\n", str[i]);
+        count++;
     }
+
+    printf("Number of characters: %d\n", count);
 
     return 0;
 }
